c9731e3f779025b07ddec997e458e0e77d9969ef
Brian P. Holt <bholt@dwolla.com>
2016-09-20 16:54:42 -0500

    add implicit class that introduces an AsyncHandler completing a Scala Promise

--- src/test/scala/com/dwolla/awssdk/utils/ScalaAsyncHandlerSpec.scala
+++ src/test/scala/com/dwolla/awssdk/utils/ScalaAsyncHandlerSpec.scala
@@ -1,11 +1,13 @@
 package com.dwolla.awssdk.utils
 
 import com.amazonaws.AmazonWebServiceRequest
+import com.amazonaws.handlers.AsyncHandler
 import org.specs2.concurrent.ExecutionEnv
+import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 import org.specs2.specification.Scope
 
-class ScalaAsyncHandlerSpec(implicit val executionEnv: ExecutionEnv) extends Specification {
+class ScalaAsyncHandlerSpec(implicit val executionEnv: ExecutionEnv) extends Specification with Mockito {
 
   trait Setup extends Scope {
     val testClass = new ScalaAsyncHandler[FakeAmazonWebServiceRequest, FakeResponse]
@@ -26,8 +28,38 @@ class ScalaAsyncHandlerSpec(implicit val executionEnv: ExecutionEnv) extends Spe
     }
   }
 
+  "Implicits.RequestHolder" should {
+    import ScalaAsyncHandler.Implicits._
+
+    "convert AmazonWebServicesRequest to a Future[Response]" in {
+      val fakeClient = new FakeAmazonAsyncClient(Right(FakeResponse("success!")))
+
+      val output = new FakeAmazonWebServiceRequest().to[FakeResponse].via(fakeClient.fakeAsync)
+
+      output must be_==(FakeResponse("success!")).await
+    }
+
+    "capture exceptions from passed method" in {
+      val fakeClient = new FakeAmazonAsyncClient(Left(IntentionalTestException))
+
+      val output = new FakeAmazonWebServiceRequest().to[FakeResponse].via(fakeClient.fakeAsync)
+
+      output must throwA(IntentionalTestException).await
+    }
+  }
 }
 
 class FakeAmazonWebServiceRequest extends AmazonWebServiceRequest
 case class FakeResponse(value: Any)
 case object IntentionalTestException extends RuntimeException("exception intentionally thrown by test", null, true, false)
+
+class FakeAmazonAsyncClient(expectedResponse: Either[Exception, FakeResponse]) {
+  def fakeAsync(req: FakeAmazonWebServiceRequest, handler: AsyncHandler[FakeAmazonWebServiceRequest, FakeResponse]): java.util.concurrent.Future[FakeResponse] = {
+    expectedResponse match {
+      case Left(ex) ⇒ handler.onError(ex)
+      case Right(res) ⇒ handler.onSuccess(req, res)
+    }
+
+    null
+  }
+}
